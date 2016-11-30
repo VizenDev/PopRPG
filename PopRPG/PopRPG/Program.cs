79a79e742f8a2dcd3ba94f312a1547cdaf364954
Vizen <afileexplorer@gmail.com>
2016-11-29 22:42:24 -0500

    Initial commit

---
+++ PopRPG/PopRPG/Program.cs
@@ -0,0 +1,52 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PopRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Start();
+        }
+
+        private DiscordClient client;
+        private string botToken = Console.ReadLine();
+        public void Start()
+        {
+            client = new DiscordClient(c =>
+            {
+                c.AppName = "Masato Bot";
+                c.LogLevel = LogSeverity.Error;
+                c.LogHandler = Log;
+            });
+            client.ExecuteAndWait(async () =>
+            {
+                await client.Connect(botToken, TokenType.Bot);
+                Console.Clear();
+                Console.Title = string.Format($"Currently connected as a {"BOT"}.");
+                client.UsingCommands(c =>
+                {
+                    c.PrefixChar = '*';
+                    c.AllowMentionPrefix = false;
+                    c.IsSelfBot = false;
+                    c.HelpMode = HelpMode.Public;
+                });
+                client.UsingModules();
+                client.AddModule<self>("self", ModuleFilter.None);
+            });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine($"[{e.Severity}] [{e.Exception}] [{e.Source}] {e.Message}");
+        }
+    }
+}
---
+++ PopRPG/PopRPG/self.cs
@@ -0,0 +1,69 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Modules;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PopRPG
+{
+    internal class self : IModule
+    {
+        private DiscordClient client;
+        private ModuleManager mod;
+        private RuntimeEnvironment re;
+        private Random rnd = new Random();
+
+        void IModule.Install(ModuleManager manager)
+        {
+            mod = manager;
+            client = manager.Client;
+
+            manager.CreateCommands("", c =>
+            {
+                c.CreateCommand("poprpg").Alias("poprpg", "rpg")
+                .Parameter("text", ParameterType.Unparsed)
+                .Do(async (e) =>
+               {
+                   string location = "rpg/PopRPG/" + e.User.Id + ".txt";
+                   if (!File.Exists(location))
+                   {
+                       Message message = await e.Channel.SendMessage("Welcome, " + e.User.Mention + "! PopRPG is a little side project, but I hope you will enjoy it. For instructions, type: **```*explain poprpg```**");
+                       StringBuilder sb = new StringBuilder();
+                       sb.AppendLine("Weapon: Potato Sword[+1]");
+                       sb.AppendLine("Armor: Potato Armor[+0.5]");
+                       sb.AppendLine("XP:0");
+                       sb.AppendLine("Level: 1");
+                       sb.AppendLine("20/20");
+                       writeFile(location, sb.ToString());
+                   }
+               });
+            });
+        }
+        private string readLine(string fileName, int lineNumber)
+        {
+            using (Stream stream = File.Open($"{fileName}", FileMode.Open))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line = null;
+                    for (int i = 0; i < lineNumber; ++i)
+                    {
+                        line = reader.ReadLine();
+                    }
+                    return line;
+                }
+            }
+        }
+
+        private void writeFile(string fileName, string text)
+        {
+            StreamWriter sWrite = new StreamWriter($"{fileName}");
+            sWrite.Write(text);
+            sWrite.Close();
+        }
+    }
+}
